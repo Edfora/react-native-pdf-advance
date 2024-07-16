2123f693e5ee7ddb0f8deb8d153986d6a419aebe
Rahul <rahul@Rahuls-MacBook-Pro.local>
2024-07-16 19:14:31 +0530

    handle crash when pdf is not loaded

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ export default class Pdf extends Component {
     componentWillUnmount() {
         this._mounted = false;
         if (this.lastRNBFTask) {
-            this.lastRNBFTask.cancel(err => {
+            this.lastRNBFTask?.cancel?.(err => {
             });
             this.lastRNBFTask = null;
         }
